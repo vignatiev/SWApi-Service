60fbb19188ceeb10232a3cfe551189c70c299668
Владислав Игнатьев <vignatiev101@gmail.com>
2019-01-19 09:08:26 +0300

    Initial Commit

---
+++ starWars_arcsinus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  starWars_arcsinus
+//
+//  Created by Владислав Игнатьев on 19.01.2019.
+//  Copyright © 2019 Владислав Игнатьев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
